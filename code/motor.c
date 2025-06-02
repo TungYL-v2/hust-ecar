b07de6eb17507cfc89e56a19a62cf31c9a48b146
ttyala <hust_TungLY@outlook.com>
2025-06-02 20:47:46 +0800

    little updata

--- code/motor.c
+++ code/motor.c
@@ -1,35 +1,46 @@
 #include "motor.h"
 
+#define MOTOR_AIN1_PIN Pin_6	//PA6
+#define MOTOR_AIN2_PIN Pin_7	//PA7
+#define MOTOR_BIN1_PIN Pin_0	//PB0
+#define MOTOR_BIN2_PIN Pin_1	//PB1
+
 uint8_t motorA_dir = 1; // 1为正转 0为反转
-uint8_t motorB_dir = 1; // 1为正转 0为反转
+uint8_t motorB_dir = 1;
 
 int Encoder_count1 = 0;
 int Encoder_count2 = 0;
 
 int speed_now;
-void motor_init()   //电机初始化
+void motor_init()   //初始化motorIO
 {
 	pwm_init(TIM_2,TIM2_CH1,1000);   //电机1
-	gpio_init(GPIO_A,Pin_6,OUT_PP);
-	gpio_init(GPIO_A,Pin_7,OUT_PP);
+	gpio_init(GPIO_A,MOTOR_AIN1_PIN,OUT_PP);
+	gpio_init(GPIO_A,MOTOR_AIN2_PIN,OUT_PP);
 	
 	pwm_init(TIM_2,TIM2_CH2,1000);   //电机2
-	gpio_init(GPIO_B,Pin_0,OUT_PP);
-	gpio_init(GPIO_B,Pin_1,OUT_PP);
-}
- 
-void motorA_duty(int duty)   //电机1占空比
-{ 
-	pwm_update(TIM_2,TIM2_CH1,duty);  
-	gpio_set(GPIO_A,Pin_6,motorA_dir);
-	gpio_set(GPIO_A,Pin_7,!motorA_dir);
+
+	gpio_init(GPIO_B,MOTOR_BIN1_PIN,OUT_PP);
+	gpio_init(GPIO_B,MOTOR_BIN2_PIN,OUT_PP);
 }
 
-void motorB_duty(int duty)   //电机2占空比
+//-------------------------------------------------------------------------------------------------------------------
+// @brief		控制转速
+// @param	  duty	  pwm占比，max50000
+// @return		void  
+//-------------------------------------------------------------------------------------------------------------------
+void motorA_duty(int duty)   //MOTOR_LEFT
+{ 
+	pwm_update(TIM_2,TIM2_CH1,duty);  
+	gpio_set(GPIO_A,MOTOR_AIN1_PIN,motorA_dir);
+	gpio_set(GPIO_A,MOTOR_AIN2_PIN,!motorA_dir);
+}
+
+void motorB_duty(int duty)   //MOTOR_RIGHT
 {
 	pwm_update(TIM_2,TIM2_CH2,duty);  
-	gpio_set(GPIO_B,Pin_0,motorB_dir);
-	gpio_set(GPIO_B,Pin_1,!motorB_dir);
+	gpio_set(GPIO_B,MOTOR_BIN1_PIN,motorB_dir);
+	gpio_set(GPIO_B,MOTOR_BIN2_PIN,!motorB_dir);
 }
 
 
--- user/main.c
+++ user/main.c
@@ -1,11 +1,26 @@
 #include "headfile.h"
 
+/*
+PA0 -> MOTOR_PWMA
+PA1 -> MOTOR_PWMB
+
+PA2 -> OUTPUT_E1A
+PA3 -> OUTPUT_E1B
+
+PA6 -> MOTOR_AIN1
+PA7 -> MOTOR_AIN2
+
+PB0 -> MOTOR_BIN1
+PB1 -> MOTOR_BIN2
+*/
+
+
 int main(void)
 {
 	OLED_Init();
 	motor_init();
-	motorA_duty(20000);
-	motorB_duty(19840);
+	motorA_duty(20000);	//LEFT SPEED (MAX:50000)
+	motorB_duty(19840); //RIGHT
 
 	while (1)
 	{
