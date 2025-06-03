00af0a59464b6ff5effb5925d12ddd9288f8ac72
ttyala <hust_TungLY@outlook.com>
2025-06-03 23:35:29 +0800

    up

--- code/motor.c
+++ code/motor.c
@@ -15,13 +15,13 @@ int speed_now;
 void motor_init()   //初始化motorIO
 {
 	pwm_init(TIM_2,TIM2_CH1,1000);   //电机1
-	gpio_init(GPIO_A,MOTOR_AIN1_PIN,OUT_PP);
-	gpio_init(GPIO_A,MOTOR_AIN2_PIN,OUT_PP);
+	gpio_init(GPIO_A,Pin_6,OUT_PP);
+	gpio_init(GPIO_A,Pin_7,OUT_PP);
 	
 	pwm_init(TIM_2,TIM2_CH2,1000);   //电机2
 
-	gpio_init(GPIO_B,MOTOR_BIN1_PIN,OUT_PP);
-	gpio_init(GPIO_B,MOTOR_BIN2_PIN,OUT_PP);
+	gpio_init(GPIO_B,Pin_0,OUT_PP);
+	gpio_init(GPIO_B,Pin_1,OUT_PP);
 }
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -32,18 +32,18 @@ void motor_init()   //初始化motorIO
 void motorA_duty(int duty)   //MOTOR_LEFT
 { 
 	pwm_update(TIM_2,TIM2_CH1,duty);  
-	gpio_set(GPIO_A,MOTOR_AIN1_PIN,motorA_dir);
-	gpio_set(GPIO_A,MOTOR_AIN2_PIN,!motorA_dir);
+	gpio_set(GPIO_A,Pin_6,motorA_dir);
+	gpio_set(GPIO_A,Pin_7,!motorA_dir);
 }
 
 void motorB_duty(int duty)   //MOTOR_RIGHT
 {
 	pwm_update(TIM_2,TIM2_CH2,duty);  
-	gpio_set(GPIO_B,MOTOR_BIN1_PIN,motorB_dir);
-	gpio_set(GPIO_B,MOTOR_BIN2_PIN,!motorB_dir);
+	gpio_set(GPIO_B,Pin_0,motorB_dir);
+	gpio_set(GPIO_B,Pin_1,!motorB_dir);
 }
 
-
+// 编码器IO初始化
 void encoder_init()
 {
 	exti_init(EXTI_PA2,FALLING,0);
